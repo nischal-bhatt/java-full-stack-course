a43035e4729590c87c1a1bdb82f5ccd81ea2d595
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-10-22 19:09:00 +0800

    committing ternary

---
+++ Full stack Java Developer Course/src/TernaryOperator.java
@@ -0,0 +1,17 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int r = 10;
+		int g = 22;
+		
+		String bigger= (r>g)? "bigger": "smaller";
+		
+		System.out.println(bigger);
+		
+		
+	}
+}
