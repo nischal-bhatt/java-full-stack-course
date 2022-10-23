e8b5980c9b7c8244bc5293e7df4c2a8b6c584242
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-10-23 23:11:46 +0800

    volatilekeyword

---
+++ Full stack Java Developer Course/src/multithreading/VolatileKeyword.java
@@ -0,0 +1,42 @@
+package multithreading;
+
+public class VolatileKeyword {
+
+	volatile public static int flag = 0;
+
+	public static void main(String[] args) {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (true) {
+					if (flag == 0) {
+
+						System.out.println("running");
+					} else {
+                            break;
+					}
+				}
+			}
+
+		}).start();
+		
+		
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				VolatileKeyword.flag = 1;
+				System.out.println("flag valuye updated");
+			}
+			
+		}).start();
+	}
+}
