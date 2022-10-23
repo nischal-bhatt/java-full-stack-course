b7fee02388f719b34089c729582d8d18275540c5
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-10-23 19:30:34 +0800

    packages

---
+++ Full stack Java Developer Course/src/org/studyeasy/PackagesExample.java
@@ -0,0 +1,7 @@
+package org.studyeasy;
+
+public class PackagesExample {
+ public static void main(String[] args) {
+	System.out.println("hey");
+}
+}
---
+++ Full stack Java Developer Course/src/org/studyhard/PackagesExample.java
@@ -0,0 +1,8 @@
+package org.studyhard;
+
+public class PackagesExample {
+
+	public static void main(String[] args) {
+		System.out.println("classes with same name");
+	}
+}
