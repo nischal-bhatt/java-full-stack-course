5ce0d4b44bd8611789774cd9d8d032ccb19b9a96
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-10-23 16:41:32 +0800

    userinput

---
+++ Full stack Java Developer Course/src/UserInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+	    System.out.println(name);
+	}
+}
